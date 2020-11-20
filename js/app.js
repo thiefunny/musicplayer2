cbd4b4ede48c23abecff3493763be72819f31f46
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-20 11:11:21 +0100

    przeróbka metod na prototype'y

--- js/app.js
+++ js/app.js
@@ -26,64 +26,68 @@ function Module(moduleId) {
   this.audioEl = document.createElement("audio");
   this.timerInterval;
   this.moduleNumber = moduleId + 1;
+}
 
-  this.playModuleRender = function (songId) {
+Module.prototype.playModuleRender = function (songId) {
 
-    let playButtonRight = document.createElement("div");
-    let titleAreaEl = document.createElement("div");
-    let titleContentEl = document.createElement("div");
-    let titleAlbumEl = document.createElement("p");
-    let titleSongEl = document.createElement("p");
-    let songDurationEl = document.createElement("div");
+  let playButtonRight = document.createElement("div");
+  let titleAreaEl = document.createElement("div");
+  let titleContentEl = document.createElement("div");
+  let titleAlbumEl = document.createElement("p");
+  let titleSongEl = document.createElement("p");
+  let songDurationEl = document.createElement("div");
 
-    this.playModuleEl.classList.add("play__module__content");
-    this.playModuleEl.setAttribute("id", `playmodule${songId}`);
-    playButtonRight.classList.add("play__button__right");
-    playButtonRight.setAttribute("id", `playbuttonright${songId}`)
-    this.audioEl.setAttribute("src", `${songsArr[songId]["url"]}`)
-    titleAreaEl.classList.add("title__area");
-    titleContentEl.classList.add("title__content");
-    titleAlbumEl.classList.add("title__album");
-    titleSongEl.classList.add("title__song");
-    songDurationEl.classList.add("song__duration");
+  this.playModuleEl.classList.add("play__module__content");
+  this.playModuleEl.setAttribute("id", `playmodule${songId}`);
+  playButtonRight.classList.add("play__button__right");
+  playButtonRight.setAttribute("id", `playbuttonright${songId}`)
+  this.audioEl.setAttribute("src", `${songsArr[songId]["url"]}`)
+  titleAreaEl.classList.add("title__area");
+  titleContentEl.classList.add("title__content");
+  titleAlbumEl.classList.add("title__album");
+  titleSongEl.classList.add("title__song");
+  songDurationEl.classList.add("song__duration");
 
-    rightPanel.appendChild(this.playModuleEl);
-    this.playModuleEl.appendChild(playButtonRight);
-    playButtonRight.innerHTML = "&#9658;";
-    playButtonRight.appendChild(this.audioEl);
+  rightPanel.appendChild(this.playModuleEl);
+  this.playModuleEl.appendChild(playButtonRight);
+  playButtonRight.innerHTML = "&#9658;";
+  playButtonRight.appendChild(this.audioEl);
 
-    this.playModuleEl.appendChild(titleAreaEl);
-    titleAreaEl.appendChild(titleContentEl);
-    titleContentEl.appendChild(titleAlbumEl);
-    titleContentEl.appendChild(titleSongEl);
-    this.playModuleEl.appendChild(songDurationEl);
+  this.playModuleEl.appendChild(titleAreaEl);
+  titleAreaEl.appendChild(titleContentEl);
+  titleContentEl.appendChild(titleAlbumEl);
+  titleContentEl.appendChild(titleSongEl);
+  this.playModuleEl.appendChild(songDurationEl);
 
-    titleAlbumEl.innerHTML = `${songsArr[songId]["title"]}`
-    titleSongEl.innerHTML = `${songsArr[songId]["artist"]}`
-    songDurationEl.innerHTML = `${songsArr[songId]["time"]}`
-  };
+  titleAlbumEl.innerHTML = `${songsArr[songId]["title"]}`
+  titleSongEl.innerHTML = `${songsArr[songId]["artist"]}`
+  songDurationEl.innerHTML = `${songsArr[songId]["time"]}`
+};
 
-  this.play = function (audioArg, timerArg, moduleArg) {
-    this.playModuleEl.addEventListener("click",
-      function () {
-        console.log(moduleArg);
-        if (audioArg.paused) {
-          for (let elem of modulesArr) {
-            elem.audioEl.pause();
-          }
-          audioArg.play();
-          timerArg = setInterval(function () {
-            progressBarPercentage.style.width = `${audioArg.currentTime / audioArg.duration * 100}%`;
-          }, 50);
-        } else {
-          audioArg.pause();
-          clearInterval(timerArg);
-        }
-        moduleNumberReturned = moduleArg;
-        return moduleNumberReturned;
+Module.prototype.play = function (audioArg, timerArg, moduleArg) {
+
+  this.playModuleEl.addEventListener("click",
+  function () {
+    console.log(moduleArg);
+    if (audioArg.paused) {
+      for (let elem of modulesArr) {
+        elem.audioEl.pause();
       }
-    )
+      audioArg.play();
+      timerArg = setInterval(function () {
+        progressBarPercentage.style.width = `${audioArg.currentTime / audioArg.duration * 100}%`;
+      }, 50);
+    } else {
+      audioArg.pause();
+      clearInterval(timerArg);
+    }
+    moduleNumberReturned = moduleArg;
+    return moduleNumberReturned;
   }
+)
+
+
+
 }
 
 // Playlist (Song modules) generator
